4520402d89ceed0536f248623133b745b10c0856
Daniel Anderson <daniela@uoregon.edu>
2021-11-03 17:14:25 -0700

    default key now `redcap_key()`

--- R/get_rts_data.R
+++ R/get_rts_data.R
@@ -1,14 +1,14 @@
 #' Return full REDCap data
 #'
 #' Returns the full data across all surveys
-#' @param redcap_api_token The API token from REDCap, passed as a string
+#' @param redcap_api_token The API token from REDCap. Defaults to 
+#'   [redcap_key()]
 #' @return A data frame with the raw data
 #' @export
 
-get_rts_data <- function(redcap_api_token) {
+get_rts_data <- function(redcap_api_token = redcap_key()) {
   redcap_read(
     redcap_uri = "https://redcap.uoregon.edu/api/",
-    token = redcap_api_token,
-    format = "json"
+    token = redcap_api_token
   )$data
 }

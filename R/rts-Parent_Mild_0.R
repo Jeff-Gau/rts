c81e0eaaaf982d95d684072f73d737b99bac01c6
Daniel Anderson <daniela@uoregon.edu>
2021-11-19 13:57:13 -0800

    initial draft of `get_parent` fun

--- R/rts-Parent_Mild_0.R
+++ R/rts-Parent_Mild_0.R
@@ -1,45 +1,75 @@
-##Saves out the Parent Survey for Mild TBI at baseline
+# define the function
+#' @param df A data frame for the function to search through
+#' @param pattern The pattern to look for. Should be what the variable name
+#'   starts with.
+#' @keywords internal
+#' @noRd
+sanitize_names <- function(df, pattern) {
+  var_indices <- grep(paste0("^", pattern), names(df))
+  nms <- names(df)[var_indices]
+  out <- gsub(paste0(pattern, "_"), paste0(pattern, "0_"), nms)
+  out <- gsub("_m0", "", out)
+  gsub("___", ".", out)
+}
 
-library(tidyverse)
-library(rts)
-library(expss)
-d <- get_rts_data()
+#' Returns parent survey data
+#'
+#' @param d The full data. Output from [get_rts_data]
+#' @param schedule The assessment schedule. Defaults to \code{baseline}.
+#'   Should be one of \code{"baseline"}, \code{"2"}, \code{"4"}, \code{"8"}, 
+#'   \code{"12"}, \code{"24"}, or \code{"end"}. 
+#' @param severity The severity of the TBI. Defaults to \code{"mild/moderate"}.
+#'   Should be one of \code{"mild/moderate"} or \code{"severe"}.
+#' @export
+get_parent <- function(d, schedule = "baseline", severity = "mild/moderate") {
+  # if (severity == "mild/moderate") {
+  #   mild0_parent_survey_complete == 2
+  # } else if (severity == "severe") {
+  #   mild0_parent_survey_complete == 1
+  # }
 
-nrow(d)
-ncol(d)
+  # the arguments passed to subset will depend on the values passed to
+  # \code{schedule} and \code{severity}
+  row_select <- d$redcap_event_name == "baseline_arm_1" &
+                  d$mild0_parent_survey_complete == 2
+  col_select <- c(
+    1,
+    grep("pdem_1_m0", names(d)):grep("sssu_4_m0", names(d))
+  )
+  d2 <- d[row_select, col_select]
+
+  # d2 <- subset(
+  #   d,
+  #   redcap_event_name == "baseline_arm_1" & mild0_parent_survey_complete == 2,
+  #   select = c(record_id, pdem_1_m0:sssu_4_m0)
+  # )
+  # eventually we will automatically sanitize all names
+  nms <- unique(
+    vapply(strsplit(names(d2), "_"), function(x) x[1], FUN.VALUE = character(1)
+    )
+  )
+  nms <- nms[-1]
+  nms_sanitized <- unlist(lapply(nms, function(x) sanitize_names(d2, x)))
+
+  names(d2) <- c("id", nms_sanitized)
+  d2
+}
 
 
-#Not all cases in this file have data. Selecting only those that do have data. Select if [mild0_parent_survey_complete] = 2. Also,
-#select only variables from the Parent Survey for Mild TBI at baseline
-d2 <-subset(d,redcap_event_name=="baseline_arm_1" & mild0_parent_survey_complete==2,
-            select=c(record_id,pdem_1_m0:mild0_parent_survey_complete) )
+# nrow(d)
+# ncol(d)
 
-nrow(d2)
-ncol(d2)
+# nrow(d2)
+# ncol(d2)
 
 #Renaming and computing new variables
-
-d3 <-d2 %>%
-  mutate(severity=1) %>%
-  rename(id = record_id,
-         dem0_1 = pdem_1_m0)
-
+# ppcsi_vars <- grep("^ppcsi", names(d2))
+# tmp <- names(d2)[ppcsi_vars]
+# tmp2 <- gsub("ppcsi_", "ppcsi0_", tmp)
+# tmp2 <- gsub("_m0", "", tmp2)
 
 
-#Creating Variable Labels
-d3 = apply_labels(d3,
-                  id = "Family ID Number",
-                  severity = 'Mild or Severe BI',
-                  severity = c("Mild/Moderate" = 1, "Severe" = 2),
-                  dem0_1 ="Parent Survey: Mild: Baseline: What is your parental role?",
-                  dem0_1 =c('Mother'= 1, 'Father'= 2, 'Stepmother' = 3, 'Stepfather = 4', 'Ohter caregive/guardian' = 5)
-                  )
 
-glimpse(d3)
-
-#Saving out the data file
-
-write.csv(d3, "C:\\Users\\jeffg\\Dropbox\\3. R\\Work-UO\\CDC-RTS\\Public\\rts\\data\\ParentSurveyMild0.csv")
-
-
+# apply the function
+# sanitize_names(d2, "pclass")
 
